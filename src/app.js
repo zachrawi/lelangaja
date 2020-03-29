e8d1c989ed37dd34b82669e437fbc6ddd2ca1dd0
Zachrawi Al Farizi <zachrawi@ngoprekweb.com>
2020-03-29 14:03:31 +0700

    Init

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Lelang aja');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
